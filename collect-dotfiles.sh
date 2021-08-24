413b7b27aa07348802d070271b1cc87c5df54975
mike-odonovan <mikeodonovan3@gmail.com>
2021-08-24 10:45:52 +0100

    update

---
+++ collect-dotfiles.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cp ~/.gitconfig ~/Dotfiles/.gitconfig
+cp ~/.bashrc ~/Dotfiles/.bashrc
+cp ~/.bash_profile ~/Dotfiles/.bash_profile
+cp ~/.tmux.conf ~/Dotfiles/.tmux.conf
+cp ~/.config/alacritty/alacritty.yml ~/Dotfiles/alacritty.yml
+cp ~/.config/karabiner/karabiner.json ~/Dotfiles/karabiner.json
+cp ~/.config/nvim/init.vim ~/Dotfiles/init.vim
+cp ~/.config/ranger/rc.conf ~/Dotfiles/rc.conf
+cp ~/.config/yabai/yabairc ~/Dotfiles/yabairc
+cp ~/.config/skhd/skhdrc ~/Dotfiles/skhdrc
+
