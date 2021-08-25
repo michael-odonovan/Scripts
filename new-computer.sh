f7ef707b0058b8196eef555c32c591fc82adbbf7
mike-odonovan <mikeodonovan3@gmail.com>
2021-08-25 09:31:26 +0100

    update

---
+++ new-computer.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+mkdir ~/Documents
+
+# Dotfiles
+mkdir ~/Dotfiles
+cd ~/bin
+gh repo clone michael-odonovan/Dotfiles
+
+# Scripts
+mkdir ~/bin
+cd ~/bin
+gh repo clone michael-odonovan/Scripts
+
+# Coding-notes
+mkdir ~/Coding-notes
+cd ~/Coding-notes
+gh repo clone michael-odonovan/Coding-notes
+
+# Pictures
+mkdir ~/Pictures
+cd ~/Pictures
+gh repo clone michael-odonovan/Pictures
+
+# Js-projects
+mkdir ~/Js-projects
+cd ~/Js-projects
+gh repo clone michael-odonovan/Js-projects
+
+# eBooks
+mkdir ~/Documents/eBooks
+cd ~/Documents/eBooks
+gh repo clone michael-odonovan/eBooks
+
+# notes-writing
+mkdir ~/Documents/notes-writing
+cd ~/Documents/notes-writing
+gh repo clone michael-odonovan/notes-writing
+
+# notes-general
+mkdir ~/Documents/notes-general
+cd ~/Documents/notes-general
+gh repo clone michael-odonovan/notes-general
+
+# Coding
+mkdir ~/Coding
+cd ~/Coding
+gh repo clone michael-odonovan/Coding
+
+# Sites
+mkdir ~/Sites
+cd ~/Sites
+gh repo clone michael-odonovan/Sites
