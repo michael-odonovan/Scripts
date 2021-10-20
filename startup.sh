752b3ef9ea3e1a680ecbd47ce0165ea539d8627e
mike-odonovan <mikeodonovan3@gmail.com>
2021-10-20 11:08:17 +0100

    update

--- startup.sh
+++ startup.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-#Switches to workspace 0 [workspaces are numbered from 0]
-wmctrl -s 0
-# gnome-terminal &
-# sleep 2
+# wmctrl -s 0 = Switches to workspace 0 [workspaces are numbered from 0]
 
 wmctrl -s 2
 nautilus &
@@ -17,3 +14,4 @@ sleep 2
 wmctrl -s 0
 firefox &
 sleep 2
+
