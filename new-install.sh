6a7c36778d70815daa4fd038b6c939043eadfa1d
Micheal O'Donovan <mikeodonovan3@gmail.com>
2023-03-20 18:03:05 +0000

    update

--- new-install.sh
+++ new-install.sh
@@ -46,8 +46,7 @@ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.
 cd ~/.oh-my-zsh/custom/
 gh repo clone agkozak/zsh-z
 ### autocompletion
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 
 brew services start skhd
 brew services start yabai
