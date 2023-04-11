8bc1fa61c1593ac9b63b753c7c6f762479a067ac
Micheal O'Donovan <mikeodonovan3@gmail.com>
2023-04-11 13:22:39 +0100

    update

--- tmux-sessions.sh
+++ tmux-sessions.sh
@@ -3,10 +3,19 @@
 tmux new-session -d -s 0_planning -c ~/repos/notes/planning/
 
 tmux new-session -d -s 1_component-library -c ~/comic-relief/component-library/
+tmux new-window -d
+tmux new-window -d
+tmux new-window -d
 
 tmux new-session -d -s 2_comic-relief -c ~/comic-relief/comicrelief-contentful/
+tmux new-window -d
+tmux new-window -d
+tmux new-window -d
 
 tmux new-session -d -s 3_donate -c ~/comic-relief/react-donation/
+tmux new-window -d
+tmux new-window -d
+tmux new-window -d
 
 tmux new-session -d -s 4_nvim -c ~/.config/nvim/
 
