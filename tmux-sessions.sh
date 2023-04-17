c7b1ce3b8540144b7f545da3547b6572a7b56579
Micheal O'Donovan <mikeodonovan3@gmail.com>
2023-04-17 12:54:50 +0100

    update

--- tmux-sessions.sh
+++ tmux-sessions.sh
@@ -9,45 +9,45 @@ tmux send-keys -t $session0:1 'cd ~/repos/notes/ && v ./planning/1.today-todo.md
 # Session 1 ===============
 session1="1_component-library"
 tmux new-session -d -s $session1 -c ~/comic-relief/component-library/
-tmux rename-window -t $session1:1 'server'
+tmux rename-window -t $session1:1 'Server'
 # tmux send-keys -t $session1:1 'yarn styleguide' C-m
-tmux new-window -t $session1:2 -c ~/comic-relief/component-library/ -n 'nvim'
+tmux new-window -t $session1:2 -c ~/comic-relief/component-library/ -n 'Nvim'
 tmux send-keys -t $session1:2 'v .' C-m
-tmux new-window -t $session1:3 -c ~/comic-relief/component-library/ -n 'testing'
+tmux new-window -t $session1:3 -c ~/comic-relief/component-library/ -n 'Testing'
 tmux send-keys -t $session1:3 'v .' C-m
-tmux new-window -t $session1:4 -c ~/comic-relief/component-library/ -n 'git'
+tmux new-window -t $session1:4 -c ~/comic-relief/component-library/ -n 'Lazygit'
 tmux send-keys -t $session1:4 'lg' C-m
 tmux select-window -t $session1:1
 
 # Session 2 ===============
 session2="2_comic-relief"
 tmux new-session -d -s $session2 -c ~/comic-relief/comicrelief-contentful/
-tmux rename-window -t $session2:1 'server'
+tmux rename-window -t $session2:1 'Server'
 # tmux send-keys -t $session2:1 'yd' C-m
-tmux new-window -t $session2:2 -c ~/comic-relief/comicrelief-contentful/ -n 'nvim'
+tmux new-window -t $session2:2 -c ~/comic-relief/comicrelief-contentful/ -n 'Nvim'
 tmux send-keys -t $session2:2 'v .' C-m
-tmux new-window -t $session2:3 -c ~/comic-relief/comicrelief-contentful/ -n 'testing'
+tmux new-window -t $session2:3 -c ~/comic-relief/comicrelief-contentful/ -n 'Testing'
 tmux send-keys -t $session2:3 'v .' C-m
-tmux new-window -t $session2:4 -c ~/comic-relief/comicrelief-contentful/ -n 'git'
+tmux new-window -t $session2:4 -c ~/comic-relief/comicrelief-contentful/ -n 'Lazygit'
 tmux send-keys -t $session2:4 'lg' C-m
 tmux select-window -t $session2:1
 
 # Session 3 ===============
 session3="3_donate"
 tmux new-session -d -s $session3 -c ~/comic-relief/react-donation/
-tmux rename-window -t $session3:1 'server'
+tmux rename-window -t $session3:1 'Server'
 # tmux send-keys -t $session3:1 'ys' C-m
-tmux new-window -t $session3:2 -c ~/comic-relief/react-donation/ -n 'nvim'
+tmux new-window -t $session3:2 -c ~/comic-relief/react-donation/ -n 'Nvim'
 tmux send-keys -t $session3:2 'v .' C-m
-tmux new-window -t $session3:3 -c ~/comic-relief/react-donation/ -n 'testing'
+tmux new-window -t $session3:3 -c ~/comic-relief/react-donation/ -n 'Testing'
 tmux send-keys -t $session3:3 'v .' C-m
-tmux new-window -t $session3:4 -c ~/comic-relief/react-donation/ -n 'git'
+tmux new-window -t $session3:4 -c ~/comic-relief/react-donation/ -n 'Lazygit'
 tmux send-keys -t $session3:4 'lg' C-m
 tmux select-window -t $session3:1
 
 # Session 4 ===============
-session4="4_nvim"
-tmux new-session -d -s $session4 -c ~/.config/nvim/
+session4="4_Nvim"
+tmux new-session -d -s $session4 -c ~/.config/Nvim/
 tmux send-keys -t $session4 'v .' C-m
 tmux select-window -t $session4:1
 
