3c1811b25cf52abaaafc80cb660088937512d279
Micheal O'Donovan <mikeodonovan3@gmail.com>
2023-06-05 12:39:13 +0100

    update

--- tmux-sessions.sh
+++ tmux-sessions.sh
@@ -60,8 +60,10 @@ tmux new-session -d -s $session4 -c ~/.config/nvim/
 
 tmux send-keys -t $session4 'v .' C-m
 
-tmux select-window -t $session4:1
+tmux new-window -t $session4:2 -c ~/.config/nvim/snippets/snippets/javascript-ecosystem/react-ts.json -n 'Snippets'
+tmux send-keys -t $session4:2 'v .' C-m
 
+tmux select-window -t $session4:1
 
 # Session 5 ===============
 session5="5_my-learning-site"
