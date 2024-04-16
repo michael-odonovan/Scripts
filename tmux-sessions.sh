a7d5ac3bdf8936f93dfac3923134d120e7db2fe3
Micheal O'Donovan <mikeodonovan3@gmail.com>
2024-04-16 11:05:43 +0100

    update

--- tmux-sessions.sh
+++ tmux-sessions.sh
@@ -12,12 +12,12 @@ tmux send-keys -t $session0:1 'cd ~/repos/notes/ && v ./planning/1.today-todo.md
 # Session 1 ===============
 session1="1_component-library"
 tmux new-session -d -s $session1 -c ~/comic-relief/component-library/
-tmux rename-window -t $session1:1 'Server'
+tmux rename-window -t $session1:1 'Code'
+tmux send-keys -t $session1:1 'v .' C-m
 
-tmux new-window -t $session1:2 -c ~/comic-relief/component-library/ -n 'Code'
-tmux send-keys -t $session1:2 'v .' C-m
+tmux new-window -t $session1:2 -c ~/comic-relief/component-library/ -n 'Lazygit'
 
-tmux new-window -t $session1:3 -c ~/comic-relief/component-library/ -n 'Lazygit'
+tmux new-window -t $session1:3 -c ~/comic-relief/component-library/ -n 'Server'
 
 tmux select-window -t $session1:1
 
@@ -25,12 +25,12 @@ tmux select-window -t $session1:1
 # Session 2 ===============
 session2="2_comic-relief"
 tmux new-session -d -s $session2 -c ~/comic-relief/comicrelief-contentful/
-tmux rename-window -t $session2:1 'Server'
+tmux rename-window -t $session2:1 'Code'
+tmux send-keys -t $session2:1 'v .' C-m
 
-tmux new-window -t $session2:2 -c ~/comic-relief/comicrelief-contentful/ -n 'Code'
-tmux send-keys -t $session2:2 'v .' C-m
+tmux new-window -t $session2:2 -c ~/comic-relief/comicrelief-contentful/ -n 'Lazygit'
 
-tmux new-window -t $session2:3 -c ~/comic-relief/comicrelief-contentful/ -n 'Lazygit'
+tmux new-window -t $session2:3 -c ~/comic-relief/comicrelief-contentful/ -n 'Server'
 
 tmux select-window -t $session2:1
 
@@ -38,12 +38,12 @@ tmux select-window -t $session2:1
 # Session 3 ===============
 session3="3_donate"
 tmux new-session -d -s $session3 -c ~/comic-relief/react-donation/
-tmux rename-window -t $session3:1 'Server'
+tmux rename-window -t $session3:1 'Code'
+tmux send-keys -t $session2:1 'v .' C-m
 
-tmux new-window -t $session3:2 -c ~/comic-relief/react-donation/ -n 'Code'
-tmux send-keys -t $session3:2 'v .' C-m
+tmux new-window -t $session3:2 -c ~/comic-relief/react-donation/ -n 'Lazygit'
 
-tmux new-window -t $session3:3 -c ~/comic-relief/react-donation/ -n 'Lazygit'
+tmux new-window -t $session3:3 -c ~/comic-relief/react-donation/ -n 'Server'
 
 tmux select-window -t $session3:1
 
@@ -82,12 +82,12 @@ tmux select-window -t $session6:1
 # Session 3 ===============
 session7="7_contact_us_rebuild"
 tmux new-session -d -s $session7 -c ~/comic-relief/contact-us-2023/
-tmux rename-window -t $session7:1 'Server'
+tmux rename-window -t $session7:1 'Code'
+tmux send-keys -t $session7:1 'v .' C-m
 
-tmux new-window -t $session7:2 -c ~/comic-relief/contact-us-2023/ -n 'Code'
-tmux send-keys -t $session7:2 'v .' C-m
+tmux new-window -t $session7:2 -c ~/comic-relief/contact-us-2023/ -n 'Lazygit'
 
-tmux new-window -t $session7:3 -c ~/comic-relief/contact-us-2023/ -n 'Lazygit'
+tmux new-window -t $session7:3 -c ~/comic-relief/contact-us-2023/ -n 'Server'
 
 tmux new-window -t $session7:4 -c ~/comic-relief/contact-us-old/ -n 'Old version'
 
